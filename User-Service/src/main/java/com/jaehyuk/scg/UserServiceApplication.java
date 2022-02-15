0ee3a13db3ec2508b213dc63a2c3b290f6826541
jason07289 <jason07999@naver.com>
2022-02-15 14:44:00 +0900

    User service init

---
+++ User-Service/src/main/java/com/jaehyuk/scg/UserServiceApplication.java
@@ -0,0 +1,24 @@
+package com.jaehyuk.scg;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+	
+	@GetMapping("/info")
+    public String info(@Value("${}") String port) {
+        return "User 서비스의 기본 동작 Port: {" + port + "}";
+    }
+}
