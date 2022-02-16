f103697305a16f40b949465ddee32b991cf523f9
jason07289 <jason07999@naver.com>
2022-02-16 11:21:56 +0900

    rewrite path

--- User-Service/src/main/java/com/jaehyuk/scg/UserServiceApplication.java
+++ User-Service/src/main/java/com/jaehyuk/scg/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,8 +18,13 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 	
-	@GetMapping("/user/info")
+	@GetMapping("/info")
     public String info(@Value("${server.port}") String port) {
         return "User 서비스의 기본 동작 Port: {" + port + "}";
+    }
+	
+	@GetMapping("/auth")
+    public String auth(@RequestHeader(value = "token") String token) {
+        return "token is " + token;
     }
 }
